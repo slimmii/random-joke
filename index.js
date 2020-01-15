bdfb75b608049c3dd660a8c7aafcd3195f282ba5
Andie Similon <andie@pickmeup.be>
2020-01-15 14:00:59 +0100

    Add eslint and tests

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-var randomJoke = require('random-joke');
+const request = require('request');
 
-(async() => {
-    
-    console.log(await randomJoke.getRandomJoke());
-    
-})();
+exports.getRandomJoke = () => {
+    return new Promise((resolve, reject) => {
+        request('http://api.icndb.com/jokes/random', function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                var dataJSON = JSON.parse(body);
+                resolve(dataJSON.value.joke);
+            } else {
+                reject();
+            }
+        });
+    });
+};
+
+function test() {
+    console.log('test');
+}
